d44887f205ea588e38efd02e0ef60c9409e8ae38
Huss <hussaini.marsidi@mili.eu>
2022-12-24 10:20:52 +0800

    create frontend

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,39 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import buildspaceLogo from "../assets/buildspace-logo.png";
 
 const Home = () => {
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
+  const [userInput, setUserInput] = useState("");
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Gen.io</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Let the AI get it done</h2>
           </div>
+        </div>
+        <div className="prompt-container">
+          <textarea className="prompt-box" placeholder="start typing here" value={userInput} onChange={onUserChangedText} />
+        </div>
+        <div className="prompt-buttons">
+          <a className="generate-button" onClick={null}>
+            <div className="generate">
+              <p>Generate</p>
+            </div>
+          </a>
         </div>
       </div>
       <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a href="https://buildspace.so/builds/ai-writer" target="_blank" rel="noreferrer">
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
             <p>build with buildspace</p>

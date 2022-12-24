ecf2ecd019e79f5e7daa299a2078c649222782f8
Huss <hussaini.marsidi@mili.eu>
2022-12-24 12:31:36 +0800

    add buttons

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,31 @@ import { useState } from "react";
 import buildspaceLogo from "../assets/buildspace-logo.png";
 
 const Home = () => {
+  const [userInput, setUserInput] = useState("");
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState("");
+
   const onUserChangedText = (event) => {
-    console.log(event.target.value);
     setUserInput(event.target.value);
   };
 
-  const [userInput, setUserInput] = useState("");
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
+
   return (
     <div className="root">
       <div className="container">
@@ -25,12 +44,22 @@ const Home = () => {
           <textarea className="prompt-box" placeholder="start typing here" value={userInput} onChange={onUserChangedText} />
         </div>
         <div className="prompt-buttons">
-          <a className="generate-button" onClick={null}>
-            <div className="generate">
-              <p>Generate</p>
-            </div>
+          <a className={isGenerating ? "generate-button loading" : "generate-button"} onClick={callGenerateEndpoint}>
+            <div className="generate">{isGenerating ? <span className="loader"></span> : <p>Generate</p>}</div>
           </a>
         </div>
+        {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
+          </div>
+        )}
       </div>
       <div className="badge-container grow">
         <a href="https://buildspace.so/builds/ai-writer" target="_blank" rel="noreferrer">
